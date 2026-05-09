b21309f7e769fe3033b9a97852dd98052e4445c6
Pritiranjan Behera <pritiranjanbehera016@gmail.com>
2026-05-09 21:34:57 +0530

    Add AboutUs component with company information

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>We provide responsive web development and student management solutions.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
